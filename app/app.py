15e256f9c32e9e804e904902fc0ea7ec3cd70be5
jolagann <joachim5943@gmail.com>
2025-05-09 14:46:56 +0200

    page + route dans app

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+
+
+
+#from config import base
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+   return render_template("login.html")
+
+
+@app.route("/sign_up")
+def sing_up():
+   return render_template("sign_up.html")
+
+@app.route("/quiz")
+def quiz():
+   return render_template("quiz.html")
+@app.route("/leader_board")
+def learder_board():
+   return render_template("leader_board.html")
