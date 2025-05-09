4c8644f6e46b9f5fa96f56e7fd8833c2b8328728
Tatiana S <tatyana.skvortsova@gmail.com>
2025-05-09 15:29:11 +0200

    write and read password from DB - it works

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, redirect, url_for
 from flask_bcrypt import Bcrypt
 from config import *
 
-temp = ''
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
