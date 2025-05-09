1a299a5fbbac4c33893717c41d95f74a479b0fa0
Tatiana S <tatyana.skvortsova@gmail.com>
2025-05-09 11:45:42 +0200

    lastname, firstname

--- app/app.py
+++ app/app.py
@@ -7,18 +7,21 @@ bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
-    return ''
+    return render_template('index.html')
 
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     if request.method == "POST":
-        username = request.form.get('username')
+        lastname = request.form.get('last_name')
+        firstname = request.form.get('first_name')
         email = request.form.get('email')
         password = request.form.get('password')
         pw_hash = bcrypt.generate_password_hash(password)
-        # save username, email and pw_hash in DB
+
+        # save lastname, firstname, email and pw_hash in DB
+
         return redirect(url_for('index'))
-    return render_template('register.html')
+    return render_template('sign_up.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -26,12 +29,13 @@ def login():
     if request.method == "POST":
         username = request.form.get('username')
         password_candidate = request.form.get('password')
+
         pw_hash = 'placeholder' # load password_hash from DB
-        bcrypt.check_password_hash(pw_hash, password_candidate)
-        if True:
+
+        if bcrypt.check_password_hash(pw_hash, password_candidate):
             return redirect(url_for('index'))
         else:
-            return redirect(url_for('login'))
+            return redirect(url_for('login'))  #Password incorrect. Try again
     return render_template('login.html')
 
 
